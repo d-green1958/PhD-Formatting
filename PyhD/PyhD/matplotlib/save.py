cd56eafe285590aa85b4a0d8fe816f937bdfb7a5
dylan <dylan.green@eng.ox.ac.uk>
2025-01-21 16:17:32 +0000

    changes to save.py

--- PyhD/PyhD/matplotlib/save.py
+++ PyhD/PyhD/matplotlib/save.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import os
 
 
-def save_fig(fig, destination, name):
+def save_fig(fig, destination, name, dpi=None):
     output_path = os.path.join(destination, name)
-    fig.savefig(output_path + "_transparent", transparent=True)
-    fig.savefig(output_path, transparent=False)
+    fig.savefig(output_path + "_transparent", transparent=True, dpi=dpi)
+    fig.savefig(output_path, transparent=False, dpi=dpi)
