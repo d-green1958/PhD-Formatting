9e33c2c2cca3e5c6ecd75a9c3a31628e79c85f73
dylan <dylan.green@eng.ox.ac.uk>
2025-01-24 15:22:13 +0000

    updated __init__ files

--- PyhD/PyhD/matplotlib/__init__.py
+++ PyhD/PyhD/matplotlib/__init__.py
@@ -1,6 +1,8 @@
 from .style import apply_style
 from .save import save_fig
 from .plot_helpers.landscape_figure import landscape_fig
+from .plot_helpers.landscape_figure_filled import filled_landscape_fig
+
 
 __all__ = ["apply_style", "save_fig", "landscape_fig"]
 
--- PyhD/PyhD/matplotlib/plot_helpers/__init__.py
+++ PyhD/PyhD/matplotlib/plot_helpers/__init__.py
@@ -1,3 +1,4 @@
 from .landscape_figure import landscape_fig
+from .landscape_figure_filled import filled_landscape_fig
 
-__all__  = [landscape_fig]
+__all__  = ["landscape_fig", "filled_landscape_fig"]
