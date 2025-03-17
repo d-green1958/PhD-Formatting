a9e4ad53d023f432c6dbc881b2dd647c46d1a284
dylan <dylan.green@eng.ox.ac.uk>
2025-03-17 09:18:54 +0000

    added pdf saving

--- PyhD/PyhD/matplotlib/save.py
+++ PyhD/PyhD/matplotlib/save.py
@@ -6,3 +6,8 @@ def save_fig(fig, destination, name, dpi=None):
     output_path = os.path.join(destination, name)
     fig.savefig(output_path + "_transparent", transparent=True, dpi=dpi)
     fig.savefig(output_path, transparent=False, dpi=dpi)
+    
+    fig.savefig(output_path + ".pdf", transparent=False, dpi=dpi)
+    fig.savefig(output_path + "_transparent.pdf", transparent=True, dpi=dpi)
+    
+    
